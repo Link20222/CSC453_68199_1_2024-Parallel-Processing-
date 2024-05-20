5ab8c741d55d90d31102976a64656ee6e7b7f1f1
ABDULRAHMAN AL-MYMAN_عبدالرحمن الميمان <66333040+PYTHON01100100@users.noreply.github.com>
2024-05-20 07:11:42 +0300

    Add files via upload

---
+++ ABSTRACT TUT/OddEvenSortV1.cu
@@ -0,0 +1,67 @@
+__global__ void oddStep(int *input, int *output, int N) {
+
+	int index = BIdx.x * BDim.x + TIdx.x;
+
+	if (index % 2 == 1 && index < N -1) {
+		if (input[index] > input[index+1]) {
+			int temp = input[index];
+			input[index] = input[index+1];
+			input[index+1] = temp;
+		}
+	}
+	__synchThreads();
+	output[index] = input[index];
+
+}
+__global__ void evenStep(int *input, int *output, int N) {
+
+	int index = BIdx.x * BDim.x + TIdx.x;
+
+	if (index % 2 == 0 && index < N -1) {
+		if (input[index] > input[index+1]) {
+			int temp = input[index];
+			input[index] = input[index+1];
+			input[index+1] = temp;
+		}
+	}
+	__synchThreads();
+	output[index] = input[index];
+
+}
+
+#define N (2048*2048)
+#define THREADS_PER_BLOCK 512
+int main(void) {
+	int *a, *sorted_a; // host copies of a, b
+	int *d_input, *d_output; // device copies of a, b, c
+	int size = N * sizeof(int);
+
+	// Alloc space for device copies of a, b, c
+	cudaMalloc((void **)&d_input, size);
+	cudaMalloc((void **)&d_output, size);
+
+	// Alloc space for host copies of a, sorted_a 
+	a = (int *)malloc(size); random_ints(a, N);
+	sorted_a = (int *)malloc(size); 
+
+	// Copy inputs to device
+	cudaMemcpy(d_input, a, size, cudaMemcpyHostToDevice);
+
+	int K = (N%2==1)?(N+1) / 2:N / 2;
+
+	for (int iteration = 0; iteration < K ; iteration++) {
+		oddStep <<<N/THREADS_PER_BLOCK , THREADS_PER_BLOCK >>> (d_input, d_ouput, N);
+
+		evenStep<<<N/THREADS_PER_BLOCK , THREADS_PER_BLOCK >>> (d_ouput, d_input, N);
+		
+	}
+
+	// Copy result back to host
+	cudeaDeviceSynchronize();
+	cudaMemcpy(c, d_input, size, cudaMemcpyDeviceToHost);
+	
+	// Cleanup
+	free(a); free(sorted_a); 
+	cudaFree(d_input); cudaFree(d_output); 
+	return 0;
+}
